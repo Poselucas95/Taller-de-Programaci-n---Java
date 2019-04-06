a62e24ec46536c425487e2e23a13934774e6120e
bautiq <bautiquere@gmail.com>
2019-04-06 12:13:45 -0300

    terminados algunos de los ultimos puntos

--- src/EJ6/GastoAnual.java
+++ src/EJ6/GastoAnual.java
@@ -3,14 +3,14 @@ package EJ6;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GastosAnuales {
+public class GastoAnual {
     private List<Rubro> listaRubro;
 
-    public GastosAnuales(){
+    public GastoAnual(){
         listaRubro = new ArrayList<>();
     }
 
-    public GastosAnuales(List<Rubro> listaRubro){
+    public GastoAnual(List<Rubro> listaRubro){
        setListaRubro(listaRubro);
     }
 
@@ -55,13 +55,7 @@ public class GastosAnuales {
     }
 
 
-    public double gastoAcumulado(Mes mes){
-        double total = 0;
-        for(Rubro rubro : this.listaRubro){
-            total += rubro.getTotalGastos(mes);
-        }
-        return total;
-    }
+
 
     private double[][] consolidadoDeGastos(){
         // Por cada mes tengo un array de rubros
@@ -70,10 +64,18 @@ public class GastosAnuales {
         Mes[] meses = Mes.values();
         for(int i = 0; i< gastos.length; i++){
             for(int j = 0; i < gastos[0].length; j++){
-                gastos[i][j] = this.listaRubro.get(j).getTotalGastos(meses[i-1]);
+                gastos[i][j] = this.listaRubro.get(j).getTotalGastos(meses[i]);
             }
         }
         return  gastos;
+    }
+
+    public double gastoAcumulado(Mes mes){
+        double total = 0;
+        for(Rubro rubro : this.listaRubro){
+            total += rubro.getTotalGastos(mes);
+        }
+        return total;
     }
 
     public double gastoAcumulado(String nombreRubro){
@@ -90,21 +92,52 @@ public class GastosAnuales {
         return gastoAcum;
     }
 
+    public void informarConsumosPorMes(){
+        // Entiendo que esto pide que muestre cada consumo por mes de cada rubro
+
+        // para saber que rubro es tomo J y hago el get de la lista
+        // ya que el gasto siempre va a ser acorde al rubro porque tienen la misma posicion
+
+        double[][] consolidado = consolidadoDeGastos();
+        double cantidadMeses = Mes.values().length;
+        Mes[] meses = Mes.values();
+
+        for(int i =0; i < cantidadMeses; i++){
+            for(int j = 0; j < consolidado[0].length; j++){
+                System.out.println("El rubro: " + this.listaRubro.get(j).getNombre() +
+                        " gastó: " + consolidado[i][j] + " en el mes: " + meses[i].toString());
+            }
+        }
+
+    }
+    public void informarPromedioMensualPorRubro(){}
+
     public void informarMesMayorConsumo(){
         Mes[] meses = Mes.values();
         double valorMaximo = 0;
         List<Mes> listaMesesGanadores = new ArrayList<>();
         for (Mes mes: meses) {
-            //TODO: define logic
             double gastoAcumuladoMesIter = gastoAcumulado(mes);
             if(gastoAcumuladoMesIter > valorMaximo) {
+                valorMaximo = gastoAcumuladoMesIter;
+                listaMesesGanadores.clear();
+                listaMesesGanadores.add(mes);
+                // Si tengo ganadores viejos y este mes tiene el valorMaximo, empataron, lo agrego a la lista de ganadores
+            }else if(listaMesesGanadores.size() > 0 && gastoAcumuladoMesIter == valorMaximo){
+                listaMesesGanadores.add(mes);
+            }
+        }
+
+        if (listaMesesGanadores.size() == 1){
+            System.out.println("El mes: " + listaMesesGanadores.get(0).toString() + " tuvo el mayor consumo con: " + valorMaximo);
+        }else {
+            System.out.println("Los meses con mayor consumo de: " + valorMaximo + " fueron:");
+            System.out.println(listaMesesGanadores.toString());
+            for (Mes mes: listaMesesGanadores){
 
             }
         }
+
     }
-
-
-
-
 
 }
