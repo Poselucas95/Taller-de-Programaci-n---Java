f70f5772ca685fdabbfee1983433ce5f1fd4e5de
Bautista Querejeta <36680644+bautiq@users.noreply.github.com>
2019-04-06 23:39:14 -0300

    add comment

--- src/ejercicio5/AnioV2.java
+++ src/ejercicio5/AnioV2.java
@@ -3,6 +3,15 @@ package ejercicio5;
 public class AnioV2 {
 	
 	public int diasTranscurridos(int mes) {
+		
+		/* Sabemos que se puede hacer asi, no tuvimos tiempo de cambiarlo
+		Mes[] meses = Meses.values()  
+		int i = 0
+		while(i<mes){
+		Mes mesIter =  meses[i];
+		acum+= e.getDias();
+		i++;
+		}*/
 		int i=1, acum=0;
 		
 		for(Meses e: Meses.values()){
